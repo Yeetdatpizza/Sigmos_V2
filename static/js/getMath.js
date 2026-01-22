4de362284c92db7c042905fa2390a3e3c9c0b06c
Yeetdatpizza <118491355+Yeetdatpizza@users.noreply.github.com>
2026-01-22 05:03:44 +0000

    That little gross thing Wyatt would 100% do this broadcast

--- static/js/getMath.js
+++ static/js/getMath.js
@@ -1,3 +1,7 @@
+math.config({
+  number: 'BigNumber',
+  precision: 64
+});
 document.addEventListener('DOMContentLoaded', () => {
 
     const clear = document.getElementById('clear')
@@ -24,7 +28,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const division = document.getElementById('division')
     const leftParen = document.getElementById('left-paren')
     const rightParen = document.getElementById('right-paren')
-
+    const e = document.getElementById('e')
+    const pi = document.getElementById('pi')
+    const sin = document.getElementById('sin')
+    const cos = document.getElementById('cos')
+    const tan = document.getElementById('tan')
+    const asin = document.getElementById('asin')
+    const acos = document.getElementById('acos')
+    const atan = document.getElementById('atan')
+    const log = document.getElementById('log')
+    const ln = document.getElementById('ln')
     const mathInput = document.getElementById('math')
 
     let previousAnswer = localStorage.getItem('PreviousAnswer')
@@ -32,10 +45,11 @@ document.addEventListener('DOMContentLoaded', () => {
     mathInput.setAttribute("placeholder", previousAnswer ? "Previous Answer: " + previousAnswer : "0")
 
     function setPreviousAnswer(answer) {
+
         if (answer !== null) {
 
             if(answer.toString() == "Infinity" || answer.toString() == "-Infinity") {
-                alert("BOII I AIN\\'T DOIN TS!")
+                alert("BOII I AIN\'T DOIN TS!")
             }
 
             localStorage.setItem('PreviousAnswer', answer)
@@ -56,58 +70,44 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function doMath(mathToDo) {
 
-        for (let i = 0; i < mathToDo.length; i++) {
-
-            if (mathToDo[i] === '×') {
-                mathToDo.splice(i, 1, '*')
-            }
-
-            else if (mathToDo[i] === '÷') {
-                mathToDo.splice(i, 1, '/')
-            }
-
-            else if (mathToDo[i] === '√') {
-                mathToDo.splice(i, 1, 'Math.sqrt(')
-                i++
-            }
-
-            else if (mathToDo[i] === '^') {
-                mathToDo.splice(i, 1, '**')
-            }
-
-            else if (mathToDo[i + 1] === '(' && /[0-9.]/.test(mathToDo[i])) {
-
-                mathToDo.splice(i + 1, 0, '*')
-                i++
-
-            }
-
-            else if (mathToDo[i - 1] === ')' && /[0-9.]/.test(mathToDo[i])) {
-                mathToDo.splice(i, 0, '*')
-                i++
-            }
-        }
+        mathToDo = mathToDo.replaceAll(' ', '')
+        mathToDo = mathToDo.replaceAll('mod', '%')
+        mathToDo = mathToDo.replaceAll('^', '**')
+        mathToDo = mathToDo.replaceAll('√', 'sqrt')
+        mathToDo = mathToDo.replaceAll('π', 'pi')
 
         console.log(mathToDo)
         return mathToDo
     
     }
 
+    function makeGood(x) {
+        const bn = math.bignumber(x);
+        const eps = math.bignumber('1e-30');
+        return math.smaller(math.abs(bn), eps) ? math.bignumber(0) : bn;
+}
+
     if (clear) clear.addEventListener('click', () => {
         mathInput.value = ''
         mathInput.setAttribute("placeholder", previousAnswer ? "Previous Answer: " + previousAnswer : "0")
     })
 
-    for (const button of [one, two, three, four, five, six, seven, eight, nine, zero, decimal, plus, minus, mod, leftParen, rightParen]) {
+    for (const button of [one, two, three, four, five, six, seven, eight, nine, zero, decimal, plus, minus, log, mod, leftParen, rightParen, e, pi, power]) {
         if (button) button.addEventListener('click', () => {
             mathInput.value += button.innerText
+        })
+    }
+
+    for(const funcButton of [sin, cos, tan, asin, acos, atan, ln]) {
+        if (funcButton) funcButton.addEventListener('click', () => {
+            mathInput.value += funcButton.innerText + '('
         })
     }
 
     if (equal) equal.addEventListener('click', () => {
 
         try {
-            const result = (Math.round((eval(doMath(mathInput.value)) + Number.EPSILON) * 100) / 100).toString()
+            const result = makeGood(math.evaluate(doMath(mathInput.value))).toString()
             checkForTuff(result)
             setPreviousAnswer(result)
         }
@@ -123,16 +123,14 @@ document.addEventListener('DOMContentLoaded', () => {
         mathInput.value = mathInput.value.slice(0, -1)
     })
 
+    
+
     if (answer) answer.addEventListener('click', () => {
         if (previousAnswer !== null) {
             mathInput.value += previousAnswer
         }
     })
-
-    if (power) power.addEventListener('click', () => {
-        mathInput.value += '**'
-    })
-
+   
     if (sqrt) sqrt.addEventListener('click', () => {
         mathInput.value += '√('
     })
